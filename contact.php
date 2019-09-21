bf06311067f4f53ea4d8c5ce62c0ccd049bc05a2
Kevin Misigaro <kmisigaro@outlook.com>
2019-09-22 02:21:31 +0300

    some final changes

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_POST["submit"])){
+// Checking For Blank Fields..
+if($_POST["name"]==""||$_POST["email"]==""||$_POST["subject"]==""||$_POST["phone"]==""||$_POST["message"]==""){
+echo "Fill All Fields..";
+}else{
+// Check if the "Sender's Email" input field is filled out
+$email=$_POST['email'];
+// Sanitize E-mail Address
+$email =filter_var($email, FILTER_SANITIZE_EMAIL);
+// Validate E-mail Address
+$email= filter_var($email, FILTER_VALIDATE_EMAIL);
+if (!$email){
+echo "Invalid Sender's Email";
+}
+else{
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$headers = 'From:'. $email; // Sender's Email
+// Message lines should not exceed 70 characters (PHP rule), so wrap it
+$message = wordwrap($message, 70);
+// Send Mail By PHP Mail Function
+mail("info@masaicamp.co.tz", $subject, $message, $headers);
+header("Location: contact.html");
+}
+}
+}
+?>
--- form.php
+++ form.php
@@ -1,32 +1,31 @@
 <?php
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "info@maasaicamp.com";
+    $email_to = "kmisigaro@outlook.com";
     $email_subject = "From contact form";
 	$error_message = '';
 
 
     // validation
-    if( 
-        !isset($_POST['name']) ||
-		!isset($_POST['email']) ||
-		!isset($_POST['subject']) ||
-		!isset($_POST['phone']) ||
-		!isset($_POST['comments']))
-		
-		{
-			
-			echo "Fields are not filled properly";
-			die();
-    }
-     
+    // if(
+    // !isset($_POST['name']) ||
+		// !isset($_POST['email']) ||
+		// !isset($_POST['subject']) ||
+		// !isset($_POST['phone']) ||
+		// !isset($_POST['comments']))
+    //
+		// {
+		// 	echo "Fields are not filled properly";
+		// 	die();
+    // }
+
     $name = $_POST['name']; // required
 	$email = $_POST['email']; // required
 	$subject = $_POST['subject']; // required
 	$phone = $_POST['phone']; // required
 	$message = $_POST['comments'];
-	
-     
+
+
 $email_message = '<html>';
 $email_message = '<body>';
 $email_message = '<head>';
@@ -40,7 +39,7 @@ $email_message .= "<tr><td><strong>Subject:</strong> </td><td>" . strip_tags($_P
 $email_message .= "<tr><td><strong>Phone:</strong> </td><td>" . strip_tags($_POST['phone']) . "</td></tr>";
 $email_message .= "<tr><td><strong>Comments:</strong> </td><td>" . strip_tags($_POST['comments']) . "</td></tr>";
 $email_message .= "</table>";
-$email_message .= "</body></html>";	
+$email_message .= "</body></html>";
 
 
 $headers = "From: " . $email . "\r\n";
@@ -50,6 +49,6 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
-@mail($email_to, $email_subject, $email_message, $headers); 
+@mail($email_to, $email_subject, $email_message, $headers);
 header ("Location: index.html");
 ?>
